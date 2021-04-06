8a6ea46da3b4b80f69e2fbb8ca4333cc2a8a37b5
DELAIREManu59700 <66071482+DELAIREManu59700@users.noreply.github.com>
2021-04-06 09:30:48 +0200

    racine

---
+++ cleanAll.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Supprime tous les produits des compilations (.o, .dll, .exe)
+
+echo Nettoyage de tous les livrables.
+GenererMakefiles clean all
+
+pause
+
---
+++ rebuildAll.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Regénère les livrables (.o, .dll, .exe)
+
+echo Génère tous les livrables.
+GenererMakefiles rebuild all
+
+pause
+
